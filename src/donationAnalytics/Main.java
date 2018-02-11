c44e29facdfe514223e9a616ea3d7297467a7632
AndreyBozhko <andybozhko@gmail.com>
2018-02-11 15:24:25 -0600

    Update Main.java

--- src/donationAnalytics/Main.java
+++ src/donationAnalytics/Main.java
@@ -148,7 +148,6 @@ public class Main {
     public static void main(String[] args) throws Exception
     {
         Main solver = new Main(args[0], args[1], args[2]);
-//        Main solver = new Main("itcont.txt", "percentile.txt", "repeatdonors.txt");
         solver.performDonationAnalysis();
     }
 
