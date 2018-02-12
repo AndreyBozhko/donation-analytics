b63534592dd710f6feecb1abb973c1139da0fb15
Andrey Bozhko <andybozhko@gmail.com>
2018-02-12 16:57:54 -0600

    Update OrderedTree.java

--- src/donationAnalytics/OrderedTree.java
+++ src/donationAnalytics/OrderedTree.java
@@ -3,24 +3,12 @@ package donationAnalytics;
 
 
 /**
- * Class that implements the order statistics tree based on the RedBlackBST,
- * where the key is a (Double, Integer) pair.
+ * Wrapper class for the RedBlackBST class with specified types for keys and values.
+ * Key is a (Double, Integer) tuple.
  * The auxiliary Integer key is used to avoid collisions between equal primary Double keys.
  * The value will always be assigned 0, since only the ordering of Double keys is of interest.
  */
-public class OrderedTree {
-    
-    private final RedBlackBST<Tuple<Double, Integer>, Integer> tree;
-    
-    
-    
-    /**
-     * Initializes empty Red Black Binary Search Tree
-     */
-    public OrderedTree()
-    {
-        tree = new RedBlackBST<>();
-    }
+public class OrderedTree extends RedBlackBST<Tuple<Double, Integer>, Integer> {
     
     
     
@@ -30,38 +18,6 @@ public class OrderedTree {
      * @param key a PairKey data type
      */
     public void put(Tuple<Double, Integer> key)
-    { tree.put(key, 0); }
+    { super.put(key, 0); }
     
-    
-    
-    /**
-     * Returns the value corresponding to key
-     * @param key a PairKey data type
-     * @return value
-     */
-    public int get(Tuple<Double, Integer> key)
-    { return tree.get(key); }
-    
-    
-    
-    /**
-     * Returns k-th smallest key from the tree.
-     * Guaranteed O(log(N)) performance.
-     * @param k index of the key
-     * @return key
-     */
-    public Tuple<Double, Integer> select(int k)
-    { return tree.select(k); }
-    
-    
-    
-    /**
-     * Returns size of the tree.
-     * @return tree size
-     */
-    public int size()
-    { return tree.size(); }
-    
-    
-
 }
