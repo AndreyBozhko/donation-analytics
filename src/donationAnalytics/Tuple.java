7e4203fb700673ad27a54a47bcdda07daa274ad3
Andrey Bozhko <andybozhko@gmail.com>
2018-02-12 17:00:56 -0600

    Update Tuple.java

--- src/donationAnalytics/Tuple.java
+++ src/donationAnalytics/Tuple.java
@@ -4,12 +4,12 @@ package donationAnalytics;
 
 /**
  * Data type that consists of a pair of keys - primary and auxiliary
- * @param <Key1> primary key
- * @param <Key2> auxiliary key
+ * @param <Key1> first key
+ * @param <Key2> second key
  */
 public class Tuple<Key1 extends Comparable<Key1>, 
-		           Key2 extends Comparable<Key2>> 
-			  implements Comparable<Tuple<Key1, Key2>>{
+		   Key2 extends Comparable<Key2>> 
+		     implements Comparable<Tuple<Key1, Key2>>{
 
     private final Key1 key1;
     private final Key2 key2;
@@ -17,8 +17,8 @@ public class Tuple<Key1 extends Comparable<Key1>,
     
     /**
      * Initializes a key pair
-     * @param key1 - first (primary) key
-     * @param key2 - second (auxiliary) key
+     * @param key1 - first key
+     * @param key2 - second key
      */
     public Tuple(Key1 key1, Key2 key2)
     {
@@ -29,30 +29,28 @@ public class Tuple<Key1 extends Comparable<Key1>,
     
     /**
      * Implementation of Comparable Interface for a pair of keys.
-     * Primary key {@code key1} is compared first.
-     * Auxiliary key {@code key2} is compared second.
+     * First, the key {@code key1} is compared, then {@code key2} is compared.
      */
     public int compareTo(Tuple<Key1, Key2> that)
     {
         int cmp = this.key1.compareTo(that.key1);
         if (cmp != 0) return cmp;
         
-        cmp = this.key2.compareTo(that.key2);
-        return cmp;
+        return this.key2.compareTo(that.key2);
     }
     
     
     /**
-     * Returns primary key
-     * @return primary key {@code key1}
+     * Returns first key
+     * @return key {@code key1}
      */
     public Key1 getKey1()
     { return key1; }
     
     
     /**
-     * Returns auxiliary key
-     * @return auxiliary key {@code key2}
+     * Returns second key
+     * @return key {@code key2}
      */
     public Key2 getKey2()
     { return key2; }
